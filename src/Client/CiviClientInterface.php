81870d65e7358ac0fbb8c5a1ecc218a0f89fff70
Tim Otten <totten@civicrm.org>
2022-03-09 17:57:28 -0800

    Interface docblocks

--- src/Client/CiviClientInterface.php
+++ src/Client/CiviClientInterface.php
@@ -6,8 +6,26 @@ use React\Promise\PromiseInterface;
 
 interface CiviClientInterface {
 
+  /**
+   * Send an APIv3 request.
+   *
+   * @param string $entity
+   * @param string $action
+   * @param array $params
+   * @return \React\Promise\PromiseInterface
+   * @see \civicrm_api3()
+   */
   public function api3(string $entity, string $action, array $params = []): PromiseInterface;
 
+  /**
+   * Send an APIv4 request.
+   *
+   * @param string $entity
+   * @param string $action
+   * @param array $params
+   * @return \React\Promise\PromiseInterface
+   * @see \civicrm_api4()
+   */
   public function api4(string $entity, string $action, array $params = []): PromiseInterface;
 
 }
--- src/Client/CiviPipeClientInterface.php
+++ src/Client/CiviPipeClientInterface.php
@@ -6,8 +6,30 @@ use React\Promise\PromiseInterface;
 
 interface CiviPipeClientInterface extends CiviClientInterface {
 
-  public function login(array $params): PromiseInterface;
+  /**
+   * Set the current user.
+   *
+   * Requires `authx` on the remote system.
+   *
+   * @param array{contactId: int, userId: int, user: string, cred: string} $request
+   *   Untrusted sessions may authenticate with a `cred`.
+   *   Trusted sessions may simply set the active principal (contact or user).
+   *
+   * @return \React\Promise\PromiseInterface
+   *   Description of the authenticated party (`contactId` and/or `userId`).
+   * @see \Civi\Pipe\PublicMethods::login()
+   */
+  public function login(array $request): PromiseInterface;
 
-  public function options(array $params): PromiseInterface;
+  /**
+   * Change session options.
+   *
+   * @param array{bufferSize: int, responsePrefix: int} $request
+   * @return \React\Promise\PromiseInterface
+   *   List of updated options.
+   *   If the list of updates was empty, then return all options.
+   * @see \Civi\Pipe\PublicMethods::options()
+   */
+  public function options(array $request): PromiseInterface;
 
 }
