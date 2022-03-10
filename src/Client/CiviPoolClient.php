574c3aca10a7d5e8471cf6aa7a1755b3e82ea3b1
Tim Otten <totten@civicrm.org>
2022-03-09 22:23:02 -0800

    CiviPoolClient - Add a client which uses the PipePool

---
+++ src/Client/CiviPoolClient.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Civi\Coworker\Client;
+
+use Monolog\Logger;
+use React\Promise\PromiseInterface;
+
+class CiviPoolClient implements CiviClientInterface {
+
+  use CiviPipeClientTrait;
+
+  /**
+   * @var \Civi\Coworker\PipePool
+   */
+  protected $pipePool;
+
+  /**
+   * @var string|null
+   */
+  protected $context;
+
+  /**
+   * @var \Monolog\Logger
+   */
+  protected $logger;
+
+  /**
+   * @param \Civi\Coworker\PipePool $pipePool
+   * @param string|null $context
+   * @param \Monolog\Logger|NULL $logger
+   */
+  public function __construct(\Civi\Coworker\PipePool $pipePool, ?string $context, ?\Monolog\Logger $logger = NULL) {
+    $this->pipePool = $pipePool;
+    $this->context = $context;
+    $this->logger = $logger ?: new Logger(static::CLASS);
+  }
+
+  protected function sendJsonRpc(string $requestLine): PromiseInterface {
+    return $this->pipePool->dispatch($this->context, $requestLine);
+  }
+
+}
