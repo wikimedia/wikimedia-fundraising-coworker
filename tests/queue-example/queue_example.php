ae0681c8442e8e9357deede220a3d28c3b273657
Tim Otten <totten@civicrm.org>
2022-10-13 22:32:39 +0100

    queue_example - Add option for setting/reading runAs

--- tests/queue-example/queue_example.php
+++ tests/queue-example/queue_example.php
@@ -2,6 +2,9 @@
 
 define('QUEUE_EXAMPLE_PREFIX', 'queue_example_');
 
+/**
+ * Remove everything from the example queues.
+ */
 function queue_example_reset(): void {
   CRM_Core_DAO::executeQuery('DELETE FROM civicrm_queue WHERE name LIKE %1', [
     1 => [QUEUE_EXAMPLE_PREFIX . '%', 'String'],
@@ -11,15 +14,28 @@ function queue_example_reset(): void {
   ]);
 }
 
-function queue_example_fill(string $name, array $logValues): void {
+/**
+ *
+ * @param string $name
+ *   Name of the example queue.
+ * @param array $logValues
+ *   List of values to put in.
+ * @param array|NULL $runAs
+ *   (Optional) The contact/domain which runs the tasks
+ *   See `CRM_Queue_Task::$runAs`.
+ *   Ex: ['contactId' => 123, 'domainId' => 1]
+ */
+function queue_example_fill(string $name, array $logValues, $runAs = NULL): void {
   /** @var CRM_Queue_Queue $queue */
   $queue = Civi::queue(QUEUE_EXAMPLE_PREFIX . $name, [
     'type' => 'SqlParallel',
     'runner' => 'task',
     'error' => 'delete',
   ]);
-  foreach ($logValues as $logValue) {
-    $queue->createItem(new CRM_Queue_Task('queue_example_logme', [$logValue]));
+  foreach ($logValues as $offset => $logValue) {
+    $task = new CRM_Queue_Task('queue_example_logme', [$logValue]);
+    $task->runAs = $runAs;
+    $queue->createItem($task);
   }
 }
 
@@ -28,7 +44,12 @@ function queue_example_logme(CRM_Queue_TaskContext $ctx, $logValue): bool {
   if (!$log || !file_exists($log) || !is_writable($log)) {
     throw new Exception('Undefined variable: QUEUE_EXAMPLE_LOG');
   }
-  $msg = json_encode(['t' => time(), 'v' => $logValue]) . "\n";
+  $msg = json_encode([
+    't' => time(),
+    'v' => $logValue,
+    'u' => CRM_Core_Session::getLoggedInContactID(),
+    'd' => CRM_Core_BAO_Domain::getDomain()->id,
+  ]) . "\n";
   file_put_contents($log, $msg, FILE_APPEND);
   return TRUE;
 }
