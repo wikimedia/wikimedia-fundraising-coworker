5cbde829db225b18c29b1caf49ffe81c652ca44b
Tim Otten <totten@civicrm.org>
2022-10-14 01:15:55 +0100

    Add TaskSplitter

---
+++ src/Util/TaskSplitter.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Civi\Coworker\Util;
+
+class TaskSplitter {
+
+  /**
+   * Split a batch of $items into contiguous sub-batches.
+   *
+   * Background:
+   *
+   * - When you `claimItems()` on a queue that supports batching, you may get multiple items
+   *   in one response.
+   * - Items can be assigned to different identities (depending on their inline data).
+   * - If tasks have different identities, then they must run separately.
+   * - Therefore, it is conceivable that you get a batch with a mix of identities,
+   *   and they should be re-split into smaller batches.
+   * - Depending on the type of queue, ordering of items may be important.
+   * - However, that should be atypical. In the average case, you would expect one batch to
+   *   have similar/consistent identities.
+   *
+   * At time writing, I regard the "typical" and "atypical" use-cases as follows:
+   *
+   * - (Typical) A batched-queue has all items running with the same identity
+   * - (Typical) A batched-queue has contiguous clusters of items with the same identity.
+   * - (Atypical) A batched-queue has items with interleaved/alternating identities
+   * - (Typical) A basic (non-batched) queue has diverse task-items (any mix of identities).
+   *
+   * The algorithm here splits the batch into sub-batches.
+   *
+   * - Each sub-batch uses exactly one identity. Identities are NEVER mixed.
+   * - If two adjacent tasks use the same identity, then they will be in the same sub-batch.
+   * - Within and across sub-batches, the relative ordering is preserved.
+   *
+   * This should be secure for all typical+atypical cases, but it may be suboptimal for
+   * some atypical cases.
+   *
+   * @param array $items
+   *   List of specific queue items.
+   *   These items correspond to `Queue.claimItems` API. They should include `id` and `run_as`.
+   *   Ex: [['id' => 123, 'run_as' => ['contactId' => 100,'domainId'=>1]]]
+   * @return array
+   *   The list of queue items, split out as various sub-batches.
+   *   Each sub-batch specifies a `context` name and some `items` to work on.
+   *   Ex: [['context' => 'c=100,d=1', 'items' => [...]]]
+   */
+  public static function split(array $items): array {
+    $subGroups = [];
+    $nextGroup = [];
+    $nextContext = NULL;
+    $acceptNext = function() use (&$subGroups, &$nextContext, &$nextGroup) {
+      if ($nextGroup) {
+        $subGroups[] = ['context' => $nextContext, 'items' => $nextGroup];
+      }
+      $nextContext = NULL;
+      $nextGroup = [];
+    };
+
+    foreach ($items as $item) {
+      $context = sprintf('c=%s,d=%s', $item['run_as']['contactId'] ?? 'null', $item['run_as']['domainId'] ?? 'null');
+      if ($nextContext !== NULL && $nextContext !== $context) {
+        $acceptNext();
+      }
+      $nextContext = $context;
+      $nextGroup[] = $item;
+    }
+    $acceptNext();
+
+    return $subGroups;
+  }
+
+}
---
+++ tests/Util/TaskSplitterTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Civi\Coworker\Util;
+
+use PHPUnit\Framework\TestCase;
+
+class TaskSplitterTest extends TestCase {
+
+  public function testNullIdentity() {
+    $input = [
+      $i1 = ['id' => '100', 'queue' => 'a', 'run_as' => NULL],
+      $i2 = ['id' => '120', 'queue' => 'a', 'run_as' => NULL],
+    ];
+    $expected = [
+      ['context' => 'c=null,d=null', 'items' => [$i1, $i2]],
+    ];
+    $actual = TaskSplitter::split($input);
+    $this->assertEquals($expected, $actual);
+  }
+
+  public function testSingleIdentity() {
+    $input = [
+      $i1 = ['id' => '200', 'queue' => 'b', 'run_as' => ['contactId' => 10, 'domainId' => 1]],
+      $i2 = ['id' => '204', 'queue' => 'b', 'run_as' => ['contactId' => 10, 'domainId' => 1]],
+      $i3 = ['id' => '214', 'queue' => 'b', 'run_as' => ['contactId' => 10, 'domainId' => 1]],
+    ];
+    $expected = [
+      ['context' => 'c=10,d=1', 'items' => [$i1, $i2, $i3]],
+    ];
+    $actual = TaskSplitter::split($input);
+    $this->assertEquals($expected, $actual);
+  }
+
+  public function testAlternateIdentities() {
+    $input = [
+      $i1 = ['id' => '201', 'queue' => 'b', 'run_as' => ['contactId' => 10, 'domainId' => 1]],
+      $i2 = ['id' => '202', 'queue' => 'b', 'run_as' => ['contactId' => 10, 'domainId' => 1]],
+      $i3 = ['id' => '203', 'queue' => 'b', 'run_as' => ['contactId' => 20, 'domainId' => 1]],
+      $i4 = ['id' => '204', 'queue' => 'b', 'run_as' => ['contactId' => 20, 'domainId' => 1]],
+      $i5 = ['id' => '205', 'queue' => 'b', 'run_as' => ['contactId' => 20, 'domainId' => 1]],
+      $i6 = ['id' => '206', 'queue' => 'b', 'run_as' => ['contactId' => 10, 'domainId' => 1]],
+      $i7 = ['id' => '207', 'queue' => 'b', 'run_as' => ['contactId' => 30, 'domainId' => 2]],
+      $i8 = ['id' => '208', 'queue' => 'b', 'run_as' => ['contactId' => 30, 'domainId' => 2]],
+    ];
+    $expected = [
+      ['context' => 'c=10,d=1', 'items' => [$i1, $i2]],
+      ['context' => 'c=20,d=1', 'items' => [$i3, $i4, $i5]],
+      ['context' => 'c=10,d=1', 'items' => [$i6]],
+      ['context' => 'c=30,d=2', 'items' => [$i7, $i8]],
+    ];
+    $actual = TaskSplitter::split($input);
+    $this->assertEquals($expected, $actual);
+  }
+
+}
